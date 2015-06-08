46f5beef485a496e2b444c23df8e8cddf7b0e8aa
Kriegslustig <github@ls7.ch>
2015-06-08 21:24:21 +0200

    Implement it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+      println!("Guess the number!");
+      let mut guess = String::new();
+      io::stdin().read_line(&mut guess)
+        .ok()
+        .expect("Failed to read your guess");
+
+      let guess: i32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+          println!("Please pass a number");
+          continue;
+        },
+      };
+
+      match guess.cmp(&secret_number) {
+        Ordering::Less => println!("To small."),
+        Ordering::Greater => println!("To big."),
+        Ordering::Equal => {
+          println!("correct");
+          break;
+        },
+      }
+    }
 }
